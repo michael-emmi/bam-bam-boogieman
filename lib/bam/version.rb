679f498d62f24a20bda61b784c654da123854375
Michael Emmi <michael.emmi@gmail.com>
2015-08-01 13:38:29 +0200

    Simplification loop.

--- lib/bam/version.rb
+++ lib/bam/version.rb
@@ -1,3 +1,3 @@
 module BAM
-  VERSION = "1.0-13-g9cc836f"
+  VERSION = "1.0-14-g4312173"
 end
--- lib/bpl/transformation/simplification.rb
+++ lib/bpl/transformation/simplification.rb
@@ -1,4 +1,120 @@
 module Bpl
+
+  module AST
+    class Node
+      def simplify
+      end
+    end
+
+    class AxiomDeclaration
+      def simplify
+        if expression.is_a?(BooleanLiteral) && expression.value == true
+          yield({
+            message: "removing trivial axiom",
+            action: Proc.new do
+              remove
+            end
+          })
+        end
+      end
+    end
+
+    class VariableDeclaration
+      def simplify
+        if bindings.all? do |b|
+          b.parent.is_a?(HavocStatement) || b.parent.is_a?(ModifiesClause)
+        end then
+          yield({
+            message: "removing unused variable",
+            action: Proc.new do
+              bindings.each do |b|
+                if b.parent.identifiers.count == 1
+                  b.parent.remove
+                else
+                  b.remove
+                end
+              end
+              remove
+            end
+          })
+        end
+      end
+    end
+
+    class ProcedureDeclaration
+      def simplify
+        if modifies.empty? && returns.empty? && body
+          yield({
+            message: "simplifying trivial procedure",
+            action: Proc.new do
+              replace_children(:body,nil)
+            end
+          })
+        end
+      end
+    end
+
+    class Block
+      def simplify
+        if statements.count == 1 &&
+           statements.first.is_a?(GotoStatement) &&
+           statements.first.identifiers.count == 1 &&
+           predecessors.count == 1 &&
+           predecessors.first.statements.last.is_a?(GotoStatement) &&
+           predecessors.first.statements.last.identifiers.count == 1
+          yield({
+            message: "removing trivial block",
+            action: Proc.new do
+              predecessors.first.statements.last.replace_with(statements.last)
+              remove
+            end
+          })
+        end
+      end
+    end
+
+    class AssertStatement
+      def simplify
+        if expression.is_a?(BooleanLiteral) && expression.value == true
+          yield({
+            message: "removing trivial assert",
+            action: Proc.new do
+              remove
+            end
+          })
+        end
+      end
+    end
+
+    class AssumeStatement
+      def simplify
+        if expression.is_a?(BooleanLiteral) && expression.value == true
+          yield({
+            message: "removing trivial assume",
+            action: Proc.new do
+              remove
+            end
+          })
+        end
+      end
+    end
+
+    class CallStatement
+      def simplify
+        decl = procedure.declaration
+        if decl.modifies.empty? && decl.returns.empty?
+          yield({
+            message: "removing trivial call",
+            action: Proc.new do
+              remove
+            end
+          })
+        end
+      end
+    end
+
+  end
+
   module Transformation
     class Simplification < Bpl::Pass
 
@@ -12,79 +128,21 @@ module Bpl
       depends :resolution, :modifies_correction, :cfg_construction
 
       def run! program
-        program.each do |elem|
-          case elem
-          when AxiomDeclaration
-            expr = elem.expression
-            if expr.is_a?(BooleanLiteral) && expr.value == true
-              info "REMOVING TRIVIAL AXIOM"
+        loop do
+          updated = false
+          program.each do |elem|
+            elem.simplify do |x|
+              info x[:message].upcase
               info elem.to_s.indent
               info
-              elem.remove
+              x[:action].call()
+              updated = true
             end
-
-          when ProcedureDeclaration
-            if elem.modifies.empty? && elem.returns.empty? && elem.body
-              info "SIMPLIFYING TRIVIAL PROCEDURE"
-              info elem.to_s.indent
-              info
-              elem.replace_children(:body,nil)
-            end
-
-          when VariableDeclaration
-            if elem.bindings.all? do |b|
-              b.parent.is_a?(HavocStatement) ||
-              b.parent.is_a?(ModifiesClause)
-            end then
-              info "REMOVING UNUSED VARIABLE"
-              info elem.to_s.indent
-              info
-              elem.bindings.each do |b|
-                if b.parent.identifiers.count == 1
-                  b.parent.remove
-                else
-                  b.remove
-                end
-              end
-              elem.remove
-            end
-
-          when AssumeStatement, AssertStatement
-            expr = elem.expression
-            if expr.is_a?(BooleanLiteral) && expr.value == true
-              info "REMOVING TRIVIAL STATEMENT"
-              info elem.to_s.indent
-              info
-              elem.remove
-            end
-
-          when Block
-            if elem.statements.count == 1 &&
-               elem.statements.first.is_a?(GotoStatement) &&
-               elem.statements.first.identifiers.count == 1 &&
-               elem.predecessors.count == 1 &&
-               elem.predecessors.first.statements.last.is_a?(GotoStatement) &&
-               elem.predecessors.first.statements.last.identifiers.count == 1
-
-              info "REMOVING TRIVIAL BLOCK"
-              info elem.to_s.indent
-              info
-              elem.predecessors.first.statements.last.replace_with(elem.statements.last)
-              elem.remove
-            end
-
-          when CallStatement
-            decl = elem.procedure.declaration
-            if decl.modifies.empty? && decl.returns.empty?
-              info "REMOVING TRIVIAL CALL"
-              info elem.to_s.indent
-              info
-              elem.remove
-            end
-
           end
+          break unless updated
         end
       end
+
     end
   end
 end
