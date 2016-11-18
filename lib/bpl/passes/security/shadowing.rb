cb347e313f47ce69d8f85cd1077ae560308771a8
Michael Emmi <michael.emmi@gmail.com>
2016-11-18 18:11:38 -0500

    Modularized shadowing; cross product vs. self comp.

--- lib/bpl/passes/security/shadowing.rb
+++ lib/bpl/passes/security/shadowing.rb
@@ -129,6 +129,221 @@ module Bpl
       bpl("$shadow_ok := $shadow_ok && #{expr};")
     end
 
+    def cross_product(proc)
+
+    end
+
+    def add_shadow_variables!(proc_decl)
+      (proc_decl.parameters + proc_decl.returns).each {|d| d.insert_after(decl(d))}
+      proc_decl.body.locals.each {|d| d.insert_after(decl(d))} if proc_decl.body
+    end
+
+    def self_composition_block(block)
+      entree, sortie = block.statements.first.get_attribute(:selfcomp)
+      return nil unless entree
+
+      block.statements.first.remove
+      shadow_block = shadow_copy(block)
+
+      equalities = Set.new
+
+      block.each do |label|
+        next unless label.is_a?(LabelIdentifier) && label.name == sortie
+        label.replace_with(LabelIdentifier.new(name: shadow(entree)))
+      end
+
+      shadow_block.replace_children(:names, *shadow_block.names.map(&method(:shadow)))
+      shadow_block.each do |label|
+        next unless label.is_a?(LabelIdentifier) && label.name != sortie
+        label.replace_with(LabelIdentifier.new(name: shadow(label.name)))
+      end
+
+      shadow_block.each do |stmt|
+        next unless stmt.is_a?(AssumeStatement)
+        next unless values = stmt.get_attribute(:branchcond)
+        expr = bpl(unshadow values.first)
+        stmt.insert_after(shadow_assert(shadow_eq(expr)))
+        equalities.add(expr)
+      end
+
+      return { block: shadow_block, before: sortie, eqs: equalities }
+    end
+
+    def cross_product_block!(block)
+      equalities = Set.new
+      arguments = Set.new
+      proc_decl = block.parent.parent
+
+      block.each do |stmt|
+        case stmt
+        when AssumeStatement
+
+          # TODO should we be shadowing assume statements?
+          # NOTE apparently not; because when they appear as branch
+          # conditions, they make the preceding shadow assertion
+          # trivially true.
+          # stmt.insert_after(shadow_copy(stmt))
+
+          # XXX this is an ugly hack to deal with memory intrinsic
+          # XXX functions which are implemented with assume statemnts
+          if magic?(proc_decl.name)
+            stmt.insert_after(shadow_copy(stmt))
+          end
+
+        when AssignStatement
+
+          # ensure the indicies to loads and stores are equal
+          accesses(stmt).each do |idx|
+            stmt.insert_before(shadow_assert(shadow_eq(idx)))
+            equalities.add(idx)
+          end
+
+          # shadow the assignment
+          stmt.insert_after(shadow_copy(stmt))
+
+        when CallStatement
+          if magic?(stmt.procedure.name)
+            stmt.arguments.each do |x|
+              unless x.type.is_a?(MapType)
+                if x.any?{|id| id.is_a?(StorageIdentifier)}
+                  stmt.insert_before(shadow_assert(shadow_eq(x)))
+                  equalities.add(x)
+                end
+              end
+            end
+          end
+
+          if exempt?(stmt.procedure.name)
+            stmt.assignments.each do |x|
+              stmt.insert_after(bpl("#{shadow(x)} := #{x};"))
+            end
+          else
+            stmt.procedure.replace_with(bpl("#{stmt.procedure}.cross_product"))
+            stmt.arguments.each do |x|
+              arguments.add(x)
+              x.insert_after(shadow_copy(x))
+            end
+            stmt.assignments.each do |x|
+              x.insert_after(shadow_copy(x))
+            end
+          end
+
+        when HavocStatement
+          nxt = stmt.next_sibling
+          if nxt and
+             nxt.is_a?(AssumeStatement)
+            nxt.insert_after(shadow_copy(nxt))
+          end
+          stmt.insert_after(shadow_copy(stmt))
+
+        when GotoStatement
+          next if stmt.identifiers.length < 2
+          unless stmt.identifiers.length == 2
+            fail "Unexpected goto statement: #{stmt}"
+          end
+
+          if annotation = stmt.previous_sibling
+            fail "Expected :branchcond annotation" unless
+              annotation.has_attribute?(:branchcond)
+
+            if expr = annotation.get_attribute(:branchcond).first
+              stmt.insert_before(shadow_assert(shadow_eq(expr)))
+              equalities.add(expr)
+            end
+          end
+
+        end
+      end
+
+      return equalities, arguments
+    end
+
+    def add_assertions!(proc_decl)
+
+      annotations = annotated_specifications(proc_decl)
+
+      # Restrict to equality on public inputs
+      annotations[:public_in].each do |annot|
+        loads(annot).each do |e,f|
+          proc_decl.append_children(:specifications,
+            bpl("requires #{e} == #{f};"))
+        end
+      end
+
+      # Restrict to equality on public / declassified outputs
+      proc_decl.body.each do |ret|
+        next unless ret.is_a?(ReturnStatement)
+
+        annotations[:public_out].each do |annot|
+          loads(annot).each do |e,f|
+            ret.insert_before(shadow_assert("#{e} == #{f}"))
+            equalities.add(e)
+          end
+        end
+
+        annotations[:declassified_out].each do |annot|
+          loads(annot).each do |e,f|
+            ret.insert_before(bpl("assume #{e} == #{f};"))
+          end
+        end
+      end
+
+      if proc_decl.has_attribute? :entrypoint
+        proc_decl.body.blocks.first.statements.first.insert_before(
+          bpl("$shadow_ok := true;"))
+        proc_decl.body.select{|r| r.is_a?(ReturnStatement)}.
+          each{|r| r.insert_before(bpl("assert $shadow_ok;"))}
+      end
+
+    end
+
+    def add_loop_invariants!(proc_decl, arguments, equalities)
+
+      equality_dependencies =
+        equalities.
+        reduce(Set.new){|acc, expr| acc | dependent_variables(proc_decl, expr)}
+
+      pointer_argument_dependencies =
+        arguments.select{|x|
+          x.is_a?(StorageIdentifier) &&
+          x.declaration &&
+          x.declaration.type.is_a?(CustomType) &&
+          x.declaration.type.name == "ref"}.
+        reduce(Set.new){|acc, expr| acc | dependent_variables(proc_decl, expr)} -
+        equality_dependencies
+
+      value_argument_dependencies =
+        arguments.select{|x|
+          x.is_a?(StorageIdentifier) &&
+          x.declaration &&
+          x.declaration.type.is_a?(CustomType) &&
+          x.declaration.type.name != "ref"}.
+        reduce(Set.new){|acc, expr| acc | dependent_variables(proc_decl, expr)} -
+        equality_dependencies -
+        pointer_argument_dependencies
+
+      loop_identification.loops.each do |head,body|
+        next unless head.parent == proc_decl.body
+        value_argument_dependencies.each do |x|
+          next unless liveness.live[head].include?(x)
+          head.prepend_children(:statements,
+            bpl("assert {:unlikely_shadow_invariant #{x} == #{shadow x}} true;"))
+        end
+        pointer_argument_dependencies.each do |x|
+          next unless liveness.live[head].include?(x)
+          head.prepend_children(:statements,
+            bpl("assert {:likely_shadow_invariant} #{x} == #{shadow x};"))
+        end
+        equality_dependencies.each do |x|
+          next unless liveness.live[head].include?(x)
+          head.prepend_children(:statements,
+            bpl("assert {:shadow_invariant} #{x} == #{shadow x};"))
+        end
+        head.prepend_children(:statements,
+          bpl("assert {:shadow_invariant} $shadow_ok;"))
+      end
+    end
+
     def run! program
 
       # duplicate global variables
@@ -140,210 +355,37 @@ module Bpl
         next unless decl.is_a?(ProcedureDeclaration)
         next if exempt?(decl.name)
 
-        annotations = annotated_specifications(decl)
+        product_decl = decl.copy
+        decl.insert_after(product_decl)
+        decl.remove_attribute(:entrypoint)
 
-        # Shadow the parameters and returns
-        (decl.parameters + decl.returns).each {|d| d.insert_after(decl(d))}
+        product_decl.replace_children(:name, "#{decl.name}.cross_product")
+        add_shadow_variables!(product_decl)
 
-        next unless decl.body
-
-        decl.body.locals.each {|d| d.insert_after(decl(d))}
+        next unless product_decl.body
 
         equalities = Set.new
         arguments = Set.new
+        block_insertions = {}
 
-        blocks_to_insert_before = {}
+        product_decl.body.blocks.each do |block|
 
-        decl.body.blocks.each do |block|
-          block.insert_before *(blocks_to_insert_before[block.name] || [])
+          block.insert_before *(block_insertions[block.name] || [])
 
-          if values = block.statements.first.get_attribute(:self_construction)
-            entree, sortie = values
-            block.statements.first.remove
-            shadow_block = shadow_copy(block)
-
-            block.each do |label|
-              next unless label.is_a?(LabelIdentifier) && label.name == sortie
-              label.replace_with(LabelIdentifier.new(name: shadow(entree)))
-            end
-
-            shadow_block.replace_children(:names, *shadow_block.names.map(&method(:shadow)))
-            shadow_block.each do |label|
-              next unless label.is_a?(LabelIdentifier) && label.name != sortie
-              label.replace_with(LabelIdentifier.new(name: shadow(label.name)))
-            end
-            shadow_block.each do |stmt|
-              next unless stmt.is_a?(AssumeStatement)
-              next unless values = stmt.get_attribute(:branchcond)
-              expr = bpl(unshadow values.first)
-              stmt.insert_after(shadow_assert(shadow_eq(expr)))
-              equalities.add(expr)
-
-            end
-
-            blocks_to_insert_before[sortie] ||= []
-            blocks_to_insert_before[sortie] << shadow_block
+          if ins = self_composition_block(block)
+            block_insertions[ins[:before]] ||= []
+            block_insertions[ins[:before]] << ins[:block]
+            equalities.merge(ins[:eqs])
 
           else
-            block.each do |stmt|
-              case stmt
-              when AssumeStatement
-
-                # TODO should we be shadowing assume statements?
-                # NOTE apparently not; because when they appear as branch
-                # conditions, they make the preceding shadow assertion
-                # trivially true.
-                # stmt.insert_after(shadow_copy(stmt))
-
-                # XXX this is an ugly hack to deal with memory intrinsic
-                # XXX functions which are implemented with assume statemnts
-                if magic?(decl.name)
-                  stmt.insert_after(shadow_copy(stmt))
-                end
-
-              when AssignStatement
-
-                # ensure the indicies to loads and stores are equal
-                accesses(stmt).each do |idx|
-                  stmt.insert_before(shadow_assert(shadow_eq(idx)))
-                  equalities.add(idx)
-                end
-
-                # shadow the assignment
-                stmt.insert_after(shadow_copy(stmt))
-
-              when CallStatement
-                if magic?(stmt.procedure.name)
-                  stmt.arguments.each do |x|
-                    unless x.type.is_a?(MapType)
-                      if x.any?{|id| id.is_a?(StorageIdentifier)}
-                        stmt.insert_before(shadow_assert(shadow_eq(x)))
-                        equalities.add(x)
-                      end
-                    end
-                  end
-                end
-
-                if exempt?(stmt.procedure.name)
-                  stmt.assignments.each do |x|
-                    stmt.insert_after(bpl("#{shadow(x)} := #{x};"))
-                  end
-                else
-                  stmt.arguments.each do |x|
-                    arguments.add(x)
-                    x.insert_after(shadow_copy(x))
-                  end
-                  stmt.assignments.each do |x|
-                    x.insert_after(shadow_copy(x))
-                  end
-                end
-
-              when HavocStatement
-                nxt = stmt.next_sibling
-                if nxt and
-                   nxt.is_a?(AssumeStatement)
-                  nxt.insert_after(shadow_copy(nxt))
-                end
-                stmt.insert_after(shadow_copy(stmt))
-
-              when GotoStatement
-                next if stmt.identifiers.length < 2
-                unless stmt.identifiers.length == 2
-                  fail "Unexpected goto statement: #{stmt}"
-                end
-
-                if annotation = stmt.previous_sibling
-                  fail "Expected :branchcond annotation" unless
-                    annotation.has_attribute?(:branchcond)
-
-                  if expr = annotation.get_attribute(:branchcond).first
-                    stmt.insert_before(shadow_assert(shadow_eq(expr)))
-                    equalities.add(expr)
-                  end
-                end
-
-              end
-            end
+            eqs, args = cross_product_block!(block)
+            equalities.merge(eqs)
+            arguments.merge(args)
           end
         end
 
-        # Restrict to equality on public inputs
-        annotations[:public_in].each do |annot|
-          loads(annot).each do |e,f|
-            decl.append_children(:specifications,
-              bpl("requires #{e} == #{f};"))
-          end
-        end
-
-        # Restrict to equality on public / declassified outputs
-        decl.body.each do |ret|
-          next unless ret.is_a?(ReturnStatement)
-
-          annotations[:public_out].each do |annot|
-            loads(annot).each do |e,f|
-              ret.insert_before(shadow_assert("#{e} == #{f}"))
-              equalities.add(e)
-            end
-          end
-
-          annotations[:declassified_out].each do |annot|
-            loads(annot).each do |e,f|
-              ret.insert_before(bpl("assume #{e} == #{f};"))
-            end
-          end
-
-        end
-
-        if decl.has_attribute? :entrypoint
-          decl.body.blocks.first.statements.first.insert_before(
-            bpl("$shadow_ok := true;"))
-          decl.body.select{|r| r.is_a?(ReturnStatement)}.
-            each{|r| r.insert_before(bpl("assert $shadow_ok;"))}
-        end
-
-        equality_dependencies =
-          equalities.
-          reduce(Set.new){|acc, expr| acc | dependent_variables(decl, expr)}
-
-        pointer_argument_dependencies =
-          arguments.select{|x|
-            x.is_a?(StorageIdentifier) &&
-            x.declaration &&
-            x.declaration.type.is_a?(CustomType) &&
-            x.declaration.type.name == "ref"}.
-          reduce(Set.new){|acc, expr| acc | dependent_variables(decl, expr)} -
-          equality_dependencies
-
-        value_argument_dependencies =
-          arguments.select{|x|
-            x.is_a?(StorageIdentifier) &&
-            x.declaration &&
-            x.declaration.type.is_a?(CustomType) &&
-            x.declaration.type.name != "ref"}.
-          reduce(Set.new){|acc, expr| acc | dependent_variables(decl, expr)} -
-          equality_dependencies -
-          pointer_argument_dependencies
-
-        loop_identification.loops.each do |head,body|
-          next unless head.parent == decl.body
-          value_argument_dependencies.each do |x|
-            next unless liveness.live[head].include?(x)
-            head.prepend_children(:statements,
-              bpl("assert {:unlikely_shadow_invariant #{x} == #{shadow x}} true;"))
-          end
-          pointer_argument_dependencies.each do |x|
-            next unless liveness.live[head].include?(x)
-            head.prepend_children(:statements,
-              bpl("assert {:likely_shadow_invariant} #{x} == #{shadow x};"))
-          end
-          equality_dependencies.each do |x|
-            next unless liveness.live[head].include?(x)
-            head.prepend_children(:statements,
-              bpl("assert {:shadow_invariant} #{x} == #{shadow x};"))
-          end
-          head.prepend_children(:statements,
-            bpl("assert {:shadow_invariant} $shadow_ok;"))
-        end
+        add_assertions!(product_decl)
+        add_loop_invariants!(product_decl, arguments, equalities)
       end
 
     end
